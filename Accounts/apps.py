51bd4769bbe389b35c0208da759c19701e978553
fayiez <fayiez@pop-os.localdomain>
2022-06-18 11:54:25 +0300

    add accounts & Delete family_associatior

--- Accounts/apps.py
+++ Accounts/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FamilyAssociatiorConfig(AppConfig):
+class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'family_associatior'
+    name = 'Accounts'
